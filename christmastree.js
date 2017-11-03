478d3e102a0bafafb18caf6c6aaa779ce7caeab7
tompng <tomoyapenguin@gmail.com>
2017-11-04 02:56:12 +0900

    blackfill

--- christmastree.js
+++ christmastree.js
@@ -5,16 +5,14 @@ function fx(x,y){
 }
 function fy(x,y){
   var th=Math.atan2(x,y)
-  return (2*Math.sqrt(4*x*x+y*y)-1)/1.4
+  return 1.8*x*x+0.65*y*y
 }
-function star(x,y,l){
-  var a=64*(x*x+y*y)
-  var b=Math.cos(5/6*Math.asin(Math.sin(9/2*Math.atan2(x,-y))))
-  return (a*b+Math.cos(l*a*b))
+function star(x,y,n,l){
+  return 64*(x*x+y*y)*Math.cos(5/6*Math.asin(Math.sin(n*Math.atan2(x,-y))))-1
 }
 function stars(x,y,a,b){
   var p=a*x+b*y, q=a*y-b*x
-  return star((Math.sqrt(p*p+1/8)-1)*2/3,2/3*(Math.sqrt(q*q+1/8)-1),8)
+  return star((Math.sqrt(p*p+1/8)-1)*3/4,(Math.sqrt(q*q+1/8)-1)*3/4,7/2)
 }
 function christmastree(x0,y0){
   var x1=fx(x0,y0),y1=fy(x0,y0)
@@ -22,7 +20,7 @@ function christmastree(x0,y0){
   var x3=fx(x2,y2),y3=fy(x2,y2)
   var x4=fx(x3,y3),y4=fy(x3,y3)
   var x5=fx(x4,y4),y5=fy(x4,y4)
-  var a=(x5*x5+y5*y5-1)*star(x0,y0-13/8,15)
-  var b=stars(x0+0.05,(y0-0.8),3,2)*stars(x0-0.1,(y0-0.5),3,-1)
+  var a=Math.atan(x5*x5+y5*y5-1)*Math.atan(star(x0,y0-1.64,9/2)*star(x0*2,(y0-1.64)*2,-9/2))-0.2
+  var b=stars(x0+0.05,(y0-0.85),3,2)*stars(x0-0.1,(y0-0.55),3,-1)
   return a*b<0
 }
