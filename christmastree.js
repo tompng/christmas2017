b6fc94cd5c28ecc6868662ed3c00f0ac61ef47ea
tompng <tomoyapenguin@gmail.com>
2017-11-04 15:25:01 +0900

    fractal star

--- christmastree.js
+++ christmastree.js
@@ -6,11 +6,13 @@ function f(p){
   }
 }
 function star(x,y,n){
-  return 80*(x*x+y*y)*Math.cos(5/6*Math.asin(Math.sin(n*Math.atan2(x,-y))))-1
+  var r2=32*(x*x+y*y)
+  var p=f(f(f(f({x:Math.sin(n*Math.atan2(x,y))*r2,y:r2+0.2}))))
+  return Math.atan(p.x*p.x+p.y*p.y-0.5)
 }
 function christmastree(x,y){
-  var p0={x:x,y:y},p1=f(p0),p2=f(p1),p3=f(p2),p4=f(p3),p5=f(p4)
-  var a=(p5.x*p5.x+p5.y*p5.y-1)*(Math.pow(star(x,y-1.64,9/2),2)-0.2)-0.2
+  var p=f(f(f(f(f(f(f(f({x:x,y:y}))))))))
+  var a=(p.x*p.x+p.y*p.y-1)*(Math.pow(star(x,y-1.64,9/2),2)-0.2)-0.2
   var b=star((Math.pow(3*x+2*y-1.5,2)-1)/3,(Math.pow(3*y-2*x-2.5,2)-1)/3,4)
   var c=star((Math.pow(3*x-y+0.4,2)-1)/3,(Math.pow(3*y+x-2,2)-1.2)/3,4)
   return a*b*c<0
